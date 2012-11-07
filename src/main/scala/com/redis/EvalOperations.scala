cd0694d08fd399cc822db6c8911ca6e0bc446154
Jonathan Siegel <jonathan@JAS-Retina.local>
2012-11-07 00:54:45 -0800

    Added evalshaBulk

--- src/main/scala/com/redis/EvalOperations.scala
+++ src/main/scala/com/redis/EvalOperations.scala
@@ -12,9 +12,11 @@ trait EvalOperations { self: Redis =>
   def evalBulk[A](luaCode: String, keys: List[Any], args: List[Any])(implicit format: Format, parse: Parse[A]): Option[A] =
     send("EVAL", argsForEval(luaCode, keys, args))(asBulk)
 
-  def evalsha[A](luaSha: String, keys: List[Any], args: List[Any])(implicit format: Format, parse: Parse[A]): Option[List[Option[A]]] =
+  def evalshaMulti[A](luaSha: String, keys: List[Any], args: List[Any])(implicit format: Format, parse: Parse[A]): Option[List[Option[A]]] =
     send("EVALSHA",  argsForEval(luaSha, keys, args))(asList[A])
 
+  def evalshaBulk[A](luaSha: String, keys: List[Any], args: List[Any])(implicit format: Format, parse: Parse[A]): Option[A] =
+    send("EVALSHA", argsForEval(luaSha, keys, args))(asBulk)
 
   private def argsForEval(luaCode: String, keys: List[Any], args: List[Any]): List[Any] =
     luaCode :: keys.length :: keys ::: args
